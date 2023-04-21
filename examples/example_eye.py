d685fd5c289e7917816fff6d4be15dc63e8ddd68
JunsuKang <cucumberkjs@gmail.com>
2023-04-21 13:31:53 +0900

    v0.1.4 indyeye example tested & uploaded

--- examples/example_eye.py
+++ examples/example_eye.py
@@ -1,11 +1,14 @@
 from neuromeka import IndyEyeClient
 from neuromeka import IndyClient
 from neuromeka.eye import *
+from neuromeka.indy_dcp.indydcp_client import *
 
-eye = IndyEyeClient("192.168.0.56")
-indy = IndyClient("192.168.0.112")
+eye = IndyEyeClient("192.168.0.100")
+indy = IndyDCPClient("192.168.0.100", "NRMK-Indy7")
 
+indy.connect()
 task_pos = indy.get_task_pos()
 object_dict = eye.get_object_dict()
 pose_obj = eye.detect(0, task_pos, mode=DetectKey.REF_TO_OBJECT)
 pose_ee = eye.detect(0, task_pos, mode=DetectKey.REF_TO_END_TOOL)
+indy.disconnect()
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="neuromeka-clients",
-    version="0.1.2",
+    version="0.1.4",
     author="Neuromeka",
     author_email="youngjin.heo@neuromeka.com",
-    description="Neuromeka client protocols for Indy, Moby, Ecat, and Motor",
+    description="Neuromeka client protocols for Indy, IndyEye, Moby, Ecat, and Motor",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/neuromeka-robotics/neuromeka-clients",
@@ -31,6 +31,7 @@ setup(
         "grpcio-tools==1.39.0",
         "protobuf==3.17.3",
         "requests",
-        "Pillow"
+        "Pillow",
+        "numpy"
     ],
 )
